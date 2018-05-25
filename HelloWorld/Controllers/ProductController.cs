e9f5d1005c8d6c07142cd12bb5f2b633a7aa8054
Justin Alemany <justin.alemany@gmail.com>
2018-05-25 11:50:10 +0200

    ajout d'une image a Son produit
    ajout de methode dans le model

--- HelloWorld/Controllers/ProductController.cs
+++ HelloWorld/Controllers/ProductController.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
+using System.Web.Mvc; // tout ce qui est asp et web
+using System.IO;// permet dacceder au system de fichier 
+using System.Web.Helpers;// pour utiliser webimage
 
 
 namespace HelloWorld.Controllers
@@ -52,7 +54,7 @@ namespace HelloWorld.Controllers
 
             }
 
-            // redirige vers la page de depart
+            // sinon redirige vers la page de depart
             return RedirectToAction("Index");
             
         }
@@ -87,6 +89,46 @@ namespace HelloWorld.Controllers
                 exist.ProductName = p.ProductName;
                 exist.ProductPrice = p.ProductPrice;
                 exist.ProductDescription = p.ProductDescription;
+
+                // request se rempli automatiquement par les methodes post ou get
+                // verifier si il y en a au moin un 
+                if(Request.Files.Count > 0)
+                {
+
+                    // recupere l'image dans file
+                    // var s'adapte au type selon le contexte (type dynamique)
+                    // recupere le premier element 
+                    // files est une collection de fichier, verifier si il y en a au moin un element 
+                    HttpPostedFileBase file = Request.Files[0];// permet de prendre le premier 
+
+                    // verifier si il est a null et sa longueur  (si il est valide) 
+                    if (file != null && file.ContentLength > 0)
+                    {
+
+                        string path = Server.MapPath("/");
+
+                        string path_img = Path.Combine(path, exist.GetImage());
+
+                        string path_th = Path.Combine(path, exist.GetThumbnail());
+
+                        file.SaveAs(path_img);
+
+                        //// creation de la variable,avec  qui son "nom" et son "extension" : par la concatenation  
+                        //var filename = exist.Reference + ".jpg" ;// remplace l'image par la nouvelle
+
+                        //// recupere le chemin du server , dynamiquement seulement en asp 
+                        //string path = Server.MapPath("~/Content/product/");
+
+                        //// chemin complet du fichier combine est une methode qui concatene 
+                        //path = Path.Combine(path,filename);
+
+                       
+                        WebImage img = new WebImage(file.InputStream);
+                        img.Resize(height: 180, width: 320);
+                        img.Save(path_th);
+
+                    }
+                }
 
             }
 
--- HelloWorld/Models/Product.cs
+++ HelloWorld/Models/Product.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;// gerer les donnees et annotation de données (accepte les attribut de methode stringlength,required,display...
+
 
 // toujours en public pour le framework et controller puisse le remplir  !!!!!!!
 namespace HelloWorld.Models
@@ -10,7 +12,13 @@ namespace HelloWorld.Models
     public class Product
     {
 
-        // nouvel syntaxe pour accesseur
+        // les modelstate (attribut de methode) sert a ne pas faire des conditions if et autres !!!
+        // nouvelle syntaxe pour les accesseur
+
+        // soit le formulair s'est valider soit ce message d'erreur 
+        [StringLength(8,ErrorMessage ="la reference est trop longue ")]// defini la longueur maximal de la reference (propriete doit faire 8 pas plus )
+        [Required(ErrorMessage= "La réference est requise")]// annotation (gere le cas ou le texte n'est pas entrer !!)
+        [Display(Name ="reference produit")]// change le texte qui est a la base le nom de la propriete !(au lieu du nom du champ le texte defini )
         public string Reference
         {
 
@@ -19,25 +27,31 @@ namespace HelloWorld.Models
 
         }
 
-
-       public string ProductDescription
+        // si il y a une erreur alors se sera celui ci
+        [Required(ErrorMessage ="La réference est requise")]
+        [StringLength(8, ErrorMessage = "la reference est trop longue ")]
+        [Display(Name = "reference produit")]// change le texte qui est a la base le nom de la propriete
+        public string ProductDescription
         {
 
-            get;
+             get;
              set;
 
         }
 
-
+        // si il y a une erreur alors se sera celui ci
+        [Display(Name = "reference produit")]// change le texte qui est a la base le nom de la propriete
         public string ProductName
         {
 
-            get;
+             get;
              set;
 
         }
 
-
+        // si il y a une erreur alors se sera celui ci
+        [Required(ErrorMessage ="La réference est requise")]
+        [Display(Name = "prix toutes taxes comprises")]// change le texte qui est a la base le nom de la propriete
         public double ProductPrice
         {
 
@@ -64,6 +78,21 @@ namespace HelloWorld.Models
 
         }
 
+        public string GetImage()
+        {
+
+            // permet de trouver et afficher la bonne image du produit choisi par l'utilisateur 
+            return "Content/product/" + Reference + ".jpg";
+        }
+
+
+        public string GetThumbnail()
+        {
+
+
+
+            return "Content/product/" + Reference + "_th.jpg";
+        }
 
     }
 }
