747e87a77c494c2b388945ce45a63c512583a2a8
Justin Alemany <justin.alemany@gmail.com>
2018-06-05 16:52:00 +0200

    debut attribut de propriete de la classe OrderLine
    Changement dans OrderLine uniquement
    PAS DE STRINGLENGHT POUR LES ID

--- HelloWorld/Models/OrderLine.cs
+++ HelloWorld/Models/OrderLine.cs
@@ -15,20 +15,28 @@ namespace HelloWorld.Models
     {
 
         // accesseur nouvelle syntaxe
-        
+        [Display(Name ="identifiant unique")]
         private int Id { get; set; }
 
-
+       
+        [Display(Name ="identifiant de la commande")]
         private int IdOrder { get; set; }
 
-
+       
+        [Display(Name ="identifiant du produit")]
         private int IdProduct { get; set; }
 
+       
+        [Required(ErrorMessage ="veuillez indiquer la quantité s'il vous plait")]
         [Display(Name ="Quantité")]
         private int Quantity { get; set; }
+        
 
+        [StringLength(8,ErrorMessage ="le prix est trop long 8max")]
+        [Required(ErrorMessage ="veuillez indiquer le prix s'il vous plait")]
         [Display(Name ="Prix")]
         private double Price { get; set; }
+
 
         public OrderLine()
         {
